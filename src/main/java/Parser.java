1b8f1aca5dc5ae70679131308ef38006864b8a8a
elektrokos@mail.ru <SatTestPunch12140>
2022-11-28 15:45:50 +0300

    Теперь парсер может публиковать  новости на сайтах Joomla v 4

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -2,12 +2,19 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Parser {
+
+    private static String siteJoomla;
+    private static String keyJoomla;
     private static String hostAddress;
     private static String log;
     private static String password;
     private static FTPLoader ftp = null;
     private  static String ftpPath;
     private static boolean ftpOn = false;
+
+    private static ParserText pt1 = null;
+
+    private static boolean joomlaOn = false;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Включить модуль FTP? да/нет :");
@@ -22,11 +29,21 @@ public class Parser {
             ftpPath = sc.nextLine();
             ftpOn = true;
         }
-        System.out.println("Введите адрес записи в вк :");
+        System.out.println("Включить модуль дабавление новости Joomla? да/нет (Joomla V4)");
+        if(sc.nextLine().equals("да")){
+            joomlaOn = true;
+            System.out.println("ВВедите Токен API :");
+            keyJoomla = sc.nextLine();
+            System.out.println("Введите адрес сайта :");
+            siteJoomla = sc.nextLine();
 
+        }
+
+        System.out.println("Введите адрес записи в вк :");
+        // тут он ждет урл формата https://vk.com/test?w=wall-11111111_1111/all
         String newUrl = urlCorector(sc.nextLine());
         while (newUrl!="exit") {
-            ParserText pt1 = new ParserText(newUrl);
+            pt1 = new ParserText(newUrl);
             ParserImage pi = new ParserImage(pt1.getUrlParsing());
             System.out.println("Имя записи :" + pt1.getNamePost());
             System.out.println("Имя файла :" + pt1.getFileName());
@@ -37,8 +54,17 @@ public class Parser {
             FileManager fl = new FileManager(pt1, new File("D:\\test1\\"), pi,ftp,ftpOn);
 
             CuterImg cuterImg = new CuterImg(fl);
+
+            if (joomlaOn){
+                RePostreJ4 postreJ4 = new RePostreJ4(keyJoomla,2,"Parser meta discription","Raz,Dva",pt1);
+                postreJ4.goPost(siteJoomla);
+            }
+
             newUrl = urlCorector(sc.nextLine());
+
+
         }
+
 
     }
     public static String urlCorector(String url){
---
+++ src/main/java/RePostreJ4.java
@@ -0,0 +1,71 @@
+import java.io.BufferedWriter;;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class RePostreJ4 {
+    private static String TOKEN_KEY = "X-Joomla-Token";
+    private static String TYPE_KEY = "Content-Type";
+    private static String TYPE_VALUE = "application/json;charset=UTF-8";
+    private static String LANGUAGE ="\"language\":\"*\"";
+    private String tokenValue;
+    private String alias;
+    private Integer catid;
+    private  String metadesc;
+    private String metakey;
+    private FTPLoader ftp;
+    private ParserText pt;
+
+
+
+    private  String title = "";
+    private String articletext= "";
+
+    public RePostreJ4(String tokenValue, Integer catid, String metadesc, String metakey,ParserText pt) {
+        this.tokenValue = tokenValue;
+        this.catid = catid;
+        this.metadesc = metadesc;
+        this.metakey = metakey;
+        this.pt = pt;
+        this.title = pt.namePost;
+        this.articletext = pt.getTextContent();
+
+        this.alias = FTPLoader.Translator.translitor(title);
+
+    }
+    public void goPost(String siteName){//Это отправляет пост запрос
+        try {
+            URL url = new URL(siteName+"/api/index.php/v1/content/articles");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            conn.setRequestProperty("Accept-Charset", "UTF-8");
+            conn.setRequestProperty("Content-Language", "ru-RU");
+
+            conn.setRequestProperty(TYPE_KEY,TYPE_VALUE);
+            conn.setRequestProperty(TOKEN_KEY,tokenValue);
+            conn.setDoOutput(true);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), "UTF-8"));
+            out.write(jsonCreater());
+            out.flush();
+            out.close();
+            System.out.println(conn.getResponseMessage());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private String jsonCreater(){ //Этот метод создает строку JSON
+        StringBuilder sb = new StringBuilder("{\"alias\":\"");
+        sb.append(alias+"\",\"articletext\":\""+clear(articletext)+"\",\"catid\":"+catid+","+LANGUAGE+",\"metadesc\":\""+metadesc
+                +"\",\"metakey\":\""+metakey+"\",\"title\": \""+title+"\"}");
+        System.out.println(sb);
+        return sb.toString();
+    }
+    private String clear(String stroka){//Это нужен так как если в строке будет " JSON сломаеться
+        return stroka.replace("\"","'");
+    }
+
+
+}
