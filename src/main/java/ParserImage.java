aae100235632fb7946eb7b40e29608da521ecec1
elektrokos@mail.ru <SatTestPunch12140>
2022-12-08 22:09:50 +0300

    Переписан метод который получает ссылки на картинки, так как прошлый перестал работать.

--- src/main/java/ParserImage.java
+++ src/main/java/ParserImage.java
@@ -25,18 +25,12 @@ public class ParserImage {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Elements title = document.select("[aria-label=\"фотография\"]");
-        /*
-        Тут я получаю строки странного вида
-        <a aria-label="фотография" onclick="return showPhoto('-162373617_457239964', 'wall-162373617_1035', {&quot;temp&quot;:{&quot;x&quot;:&quot;&quot;,&quot;y&quot;:&quot;&quot;,&quot;z&quot;:&quot;&quot;,&quot;w&quot;:&quot;&quot;,&quot;x_&quot;:[&quot;&quot;,604,453],&quot;y_&quot;:[&quot;&quot;,807,605],&quot;z_&quot;:[&quot;&quot;,1280,960],&quot;w_&quot;:[&quot;&quot;,2560,1920],&quot;base&quot;:&quot;&quot;},&quot;queue&quot;:1}, event)" style="width: 252px; height: 189px;background-image: url(https://sun9-38.userapi.com/impg/o3JQZB6buGmfXddTVT9YRyoJjYUxXuW128lA7w/gw7cRLiyYSw.jpg?size=604x453&amp;quality=95&amp;sign=dc7fb39fa0e59d954a9a4dfbb93c91d5&amp;type=album);" class="page_post_thumb_wrap image_cover  page_post_thumb_last_row fl_l page_post_thumb_not_single"></a>
-        по атрибуту ариа
-        потом пытаюсь вытащить из него url
-        и добавить в список
-        */
+        Elements title = document.select("img.MediaGrid__imageElement");
+        
         for(Element e:title){
             String urlParse = e.toString();
-            urlParse = urlParse.split("\\(")[2];
-            urlParse = urlParse.split("\\)")[0];
+            urlParse = urlParse.split("src=\"")[1];
+            urlParse = urlParse.split("\"")[0];
             urlParse = coorectUrl(urlParse);
             urlImage.add(urlParse);
         }
