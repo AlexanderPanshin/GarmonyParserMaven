5ccc7b5ea721d915bc1d438c61a9091a9f5c8633
elektrokos@mail.ru <SatTestPunch12140>
2022-12-12 21:44:37 +0300

    В класс добавлено поле ftpPath и гетер и сетер для него ибо это информация нужна в классе  FileManager. Пришлось переписать метод транслятор ибо библиотека icu ломала урлы странными символами. Теперь там длинный отваратный свитч.

--- src/main/java/FTPLoader.java
+++ src/main/java/FTPLoader.java
@@ -1,17 +1,23 @@
-import com.ibm.icu.text.Transliterator;
+
 import org.apache.commons.net.ftp.FTPClient;
 
-import javax.imageio.IIOException;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
+
 
 public class FTPLoader {
     private String hostAddress;
     private String log;
     private String password;
     private ParserText pt;
+
+    public String getFtpPath() {
+        return ftpPath;
+    }
+
     private String ftpPath;
     public FTPLoader(String hostAddress, String log, String password, ParserText pt,String ftpPath) {
         this.hostAddress = hostAddress;
@@ -44,11 +50,105 @@ public class FTPLoader {
         }
     }
     static class Translator{
-        public static final String CYRILLIC_TO_LATIN = "Cyrillic-Latin";
+        /*Переписал транслятор убрал библиотеку com.ibm.icu.text.Transliterator
+        она странно траслировала символы в какие то сатанинские иероглифы и арабскую вязь
+        а это ломало урлы. Я не хотел писать большой свитч ниже, но мне пришлось ...
+         */
         public static String translitor(String st){
-            Transliterator toLatinTrans = Transliterator.getInstance(CYRILLIC_TO_LATIN);
-            String result = toLatinTrans.transliterate(st);
-            return result;
-        }
+            st = st.toLowerCase();
+            char [] charMass = st.toCharArray();
+            for(int i = 0; i < charMass.length; i++){
+                switch (charMass[i]){
+                    case 'а':
+                        charMass[i]='a';
+                        break;
+                    case 'б':
+                        charMass[i]='b';
+                        break;
+                    case 'в':
+                        charMass[i]='v';
+                        break;
+                    case 'г':
+                        charMass[i]='g';
+                        break;
+                    case 'д':
+                        charMass[i]='d';
+                        break;
+                    case 'е':
+                    case 'э':
+                        charMass[i]='e';
+                        break;
+                    case 'ё':
+                    case 'о':
+                        charMass[i]='o';
+                        break;
+                    case 'ж':
+                        charMass[i]='z';
+                        break;
+                    case 'з':
+                        charMass[i]='z';
+                        break;
+                    case 'и':
+                    case 'ы':
+                        charMass[i]='i';
+                        break;
+                    case 'й':
+                        charMass[i]='i';
+                        break;
+                    case 'к':
+                        charMass[i]='k';
+                        break;
+                    case 'л':
+                        charMass[i]='l';
+                        break;
+                    case 'м':
+                        charMass[i]='m';
+                        break;
+                    case 'н':
+                        charMass[i]='n';
+                        break;
+                    case 'п':
+                        charMass[i]='p';
+                        break;
+                    case 'р':
+                        charMass[i]='r';
+                        break;
+                    case 'с':
+                        charMass[i]='c';
+                        break;
+                    case 'т':
+                        charMass[i]='t';
+                        break;
+                    case 'у':
+                        charMass[i]='y';
+                        break;
+                    case 'ф':
+                        charMass[i]='f';
+                        break;
+                    case 'х':
+                        charMass[i]='h';
+                        break;
+                    case 'ц':
+                    case 'ч':
+                        charMass[i]='c';
+                        break;
+                    case 'ш':
+                    case 'щ':
+                        charMass[i]='s';
+                        break;
+                    case 'ъ':
+                    case 'ь':
+                        charMass[i]='_';
+                        break;
+                    case 'ю':
+                        charMass[i]='u';
+                        break;
+                    case 'я':
+                        charMass[i]='y';
+                        break;
+                }
+            }
+            return String.valueOf(charMass);
+         }
     }
 }
