2012aae3a918e72a3f678541002bb9304bcf97ef
AlexandrFedoraPC <elektrokos@mail.ru>
2022-12-20 22:14:43 +0300

    Создан класс диалога для отправки json на  api joomla

---
+++ src/main/java/gui/DialogJoom.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javax.swing.*;
+
+public class DialogJoom {
+    public String otvetJ4() {
+        int i = JOptionPane.showConfirmDialog(null, "Включить модуль дабавление новости Joomla?Joomla V4", "Garmony Parser", 1);
+        if (i == 0) {
+            return "да";
+        }
+        if (i == 1) {
+            return "нет";
+        } else return "выход";
+    }
+    public String tokenJoomla(){
+        String otvet = JOptionPane.showInputDialog(null,"Введите Токен API","Garmony Parser",1);
+        return otvet;
+    }
+    public String urlJoomla(){
+        String otvet = JOptionPane.showInputDialog(null,"Введите адрес сайта","Garmony Parser",1);
+        return otvet;
+    }
+}
+
