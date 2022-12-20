d05c47d4866793e36f695b930ff0a4cf290d2c51
AlexandrFedoraPC <elektrokos@mail.ru>
2022-12-20 22:15:57 +0300

    Создан класс диалога для получение ссылки ВК

---
+++ src/main/java/gui/DialogVk.java
@@ -0,0 +1,22 @@
+package gui;
+
+import javax.swing.*;
+import java.io.File;
+
+public class DialogVk {
+    public String urlVk(){
+        String otvet = JOptionPane.showInputDialog(null,"Введите адрес записи в вк","Garmony Parser",1);
+        return otvet;
+    }
+    public File chooserVK(){
+        JFileChooser file = new JFileChooser();
+        file.setMultiSelectionEnabled(false);
+        file.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        file.setFileHidingEnabled(false);
+        if(file.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+            File f = file.getSelectedFile();
+            return f;
+        }
+        else return null;
+    }
+}
